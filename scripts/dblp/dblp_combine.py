394815d0e27c556bd38c3b0cff2dd1eed1dfe96b
akbnq <jan.bittner@student.uni-halle.de>
2021-04-25 01:54:28 +0200

    fix write argument

--- scripts/dblp/dblp_combine.py
+++ scripts/dblp/dblp_combine.py
@@ -5,7 +5,7 @@ import shutil
 def main(path_cache_bib, path_other_bib, path_criteria_bib, path_bundle_bib):
     with open(path_bundle_bib,"w") as bundle:
         pass 
-    with open(path_bundle_bib,"w") as bundle:
+    with open(path_bundle_bib,"wb") as bundle:
         for f in [path_cache_bib, path_other_bib, path_criteria_bib]:
             with open(f,'rb') as fd:
                 shutil.copyfileobj(fd, bundle)
