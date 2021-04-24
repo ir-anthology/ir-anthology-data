7d92328613b4523a2a30419ef51e8931a8b8f838
akbnq <jan.bittner@student.uni-halle.de>
2021-04-25 01:50:49 +0200

    fix bundling

--- scripts/dblp/dblp_combine.py
+++ scripts/dblp/dblp_combine.py
@@ -8,4 +8,4 @@ def main(path_cache_bib, path_other_bib, path_criteria_bib, path_bundle_bib):
     with open(path_bundle_bib,"w") as bundle:
         for f in [path_cache_bib, path_other_bib, path_criteria_bib]:
             with open(f,'rb') as fd:
-                shutil.copyfileobj(fd, wfd)
+                shutil.copyfileobj(fd, bundle)
