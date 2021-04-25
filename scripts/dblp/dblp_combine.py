e5424925c687527457aad32fc16223783286b149
akbnq <jan.bittner@student.uni-halle.de>
2021-04-25 02:01:26 +0200

    change combine to run on strings

--- scripts/dblp/dblp_combine.py
+++ scripts/dblp/dblp_combine.py
@@ -5,7 +5,7 @@ import shutil
 def main(path_cache_bib, path_other_bib, path_criteria_bib, path_bundle_bib):
     with open(path_bundle_bib,"w") as bundle:
         pass 
-    with open(path_bundle_bib,"wb") as bundle:
+    with open(path_bundle_bib,"w") as bundle:
         for f in [path_cache_bib, path_other_bib, path_criteria_bib]:
-            with open(f,'rb') as fd:
+            with open(f,'r') as fd:
                 shutil.copyfileobj(fd, bundle)
