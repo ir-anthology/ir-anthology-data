e54e3e83a15ecfec7a4c88323903d987b2311f6f
najtin <42641080+najtin@users.noreply.github.com>
2021-05-10 00:04:12 +0200

    add more info to command help

--- scripts/dblp/dblp_manager.py
+++ scripts/dblp/dblp_manager.py
@@ -19,14 +19,14 @@ path_other_cache = ".cache/dblp/dblp_other_cache"
 def dblp():
     pass
 
-@dblp.command(help='pull references')
+@dblp.command(help='Update references (dblp.xml) and extract changed/new entries that match the criteria - this will not change the ir-anthology.bib')
 @click.option('--skip-download', help='skips the download, only selects the xml entries based on the criteria', is_flag=True, default=False)
 def update(skip_download):
     if not skip_download:
         dblp_download.download(path_dblp)
     dblp_download.select(path_dblp, path_criteria, path_references)
 
-@dblp.command(help='pull references')
+@dblp.command(help='Crawl all extracted entries and append them and their meta data to the ir-anthology.bib - This will replace outdated entries. Only changes entries crawled from the dblp. Crawling 0.2 entries per second. ')
 def upgrade():
         dblp_cache.main(path_data, path_dblp_cache, path_other_cache)
         dblp_upgrade.main(path_dblp_cache, path_references)
