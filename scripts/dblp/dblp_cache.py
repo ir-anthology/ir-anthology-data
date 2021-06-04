d8dedd68496fd5e282cb09295c51877ad4f17771
akbnq <jan.bittner@student.uni-halle.de>
2021-06-04 14:37:52 +0200

    replace tab by whitespace

--- scripts/dblp/dblp_cache.py
+++ scripts/dblp/dblp_cache.py
@@ -5,7 +5,7 @@ def main(path_main_bib, path_cache_bib, path_other_bib):
         with open(path_cache_bib, "w") as cache_bib:
             with open(path_other_bib, "w") as other_bib:
                 for entry in bibtexparser.loads(input=main_bib.read()):
-                	bibid = entry.bibid()
+                    bibid = entry.bibid()
                     if bibid.startswith("DBLP:"):
                         cache_bib.write(entry.string())
                         cache_bib.write("\n")
