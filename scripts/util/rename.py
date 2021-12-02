d6139fd2709e53c901b69aa13fa0b696cc930eb2
akbnq <jan.bittner@student.uni-halle.de>
2021-12-02 14:28:00 +0100

    handle malformed names, years, titles

--- scripts/util/rename.py
+++ scripts/util/rename.py
@@ -2,6 +2,7 @@ import nameparser
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import unicodedata
+import re
 
 import nltk
 nltk.download('stopwords')
@@ -9,7 +10,9 @@ nltk.download('punkt')
 stop_words = set(stopwords.words('english'))
 
 def normalize_string(input_string):
-    return unicodedata.normalize('NFKD', input_string).encode('ASCII', 'ignore').decode("ASCII").replace(" ", "-")
+    string =  unicodedata.normalize('NFKD', input_string).encode('ASCII', 'ignore').decode("ASCII").replace(" ", "-")
+    string = re.sub('[^a-zA-Z]+', '', string)
+    return string
 
 def generate_bibid(bibid, fields):
     parts = []
@@ -23,14 +26,20 @@ def generate_bibid(bibid, fields):
         first_author = first_author.split(" and ")[0]
         last_name = nameparser.HumanName(first_author).last
         last_name = normalize_string(last_name)
-        parts.append(last_name)
+        if len(last_name)!=0:
+            parts.append(last_name)
     if "year" in fields:
-        parts.append(fields["year"])
+        year = re.sub('[^0-9]+', '', fields["year"])
+        if len(year)!=0:
+            parts.append(fields["year"])
     if "title" in fields:
-        first_word = list(filter(lambda x: x not in stop_words, word_tokenize(fields["title"].lower())))
-        if len(first_word)!=0:
-            first_word = normalize_string(first_word[0])
-            parts.append(first_word)
+        words = list(filter(lambda x: x not in stop_words, word_tokenize(fields["title"].lower())))
+        for first_word in words: 
+            first_word = normalize_string(first_word)
+            if len(first_word)!=0:
+                first_word = normalize_string(first_word)
+                parts.append(first_word)
+                break
     if len(parts) == 0:
         raise KeyError("Neither author, editor, year or title present in "+bibid)
     new_bibid = "-".join(parts).lower()
